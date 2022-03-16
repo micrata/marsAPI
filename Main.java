c3830340183a875c917861daa08c6f2995d2426a
Khush Thakor <khushthakor@gmail.com>
2022-03-16 13:00:12 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,12 @@
 package application;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
-
-import javax.imageio.ImageIO;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
 
 
 public class Main extends Application {
