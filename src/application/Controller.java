a816cb0731cc1248cb281c82abd0bb1b99977898
Khush Thakor <khushthakor@gmail.com>
2022-03-17 11:24:55 -0700

    Add files via upload

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -14,12 +14,9 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Hyperlink;
@@ -32,181 +29,231 @@ import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+/**
+ * JavaFX elements and ids with interactions
+ * <li> JavaFX dateInput @fx:id DatePicker - Allows user to select date from calendar.
+ *      All photoIDs from selected day are added to photoList and displayed in ListView.
+ * <li> JavaFX ListView @fx:id photoList - Displays list of photoIDs from date selected.
+ * <li> JavaFX imageView @fx:id imageV - Displays images associated with selected photoID in Listview from specified date.
+ * <li> JavaFX TextArea @fx:id imageInfo - Displays all metadata associated with selected photo. Data displayed is pulled
+ *      from JSONObject apiPhotos using keyset from photoID.
+ * <li> JavaFX TextField @fx:id imgSrcText - Displays link from String ApiKey. Returns link with image from
+ *      selected date.
+ * <li> JavaFX Button @fx:id downloadbutton - Executes method downloadImage();
+ * <li> JavaFX Button @fx:id copyButton - Executes method copyText();
+ * <li> JavaFX Hypyerlink @fx:id historybutton - Sets int mode to -1 to reverse stack Photohistory. Executes
+ *      method historyButtonM().
+ */
 public class Controller {
+    /**
+     * Private instance variables are the actual buttons and displays in the javafx application.
+     */
+    @FXML
+    private ImageView imageV;
+    @FXML
+    private DatePicker  dateInput;
+    @FXML
+    private TextArea imageInfo;
+    @FXML
+    private ListView<String> photoList;
+    @FXML
+    private TextField imgSrcText;
+    @FXML
+    private Button copyButton;
+    @FXML
+    private AnchorPane anchorpane;
+    @FXML
+    private Hyperlink historyButton = new Hyperlink();
 
-	@FXML
-	private ImageView imageV;
-	@FXML
-	private DatePicker  dateInput;
-	@FXML
-	private TextArea imageInfo;
-	@FXML
-	private ListView<String> photoList;
-	@FXML
-	private TextField imgSrcText;
-	@FXML
-	private Button copyButton;
-	@FXML
-	private AnchorPane anchorpane;
-	@FXML
-	private Hyperlink historyButton = new Hyperlink();
-	
-	
-	ArrayList<Photo>photos = new ArrayList<Photo>();
-	
-	HttpURLConnection conn;
-	
-	HashMap<String, Integer> idToPhoto = new LinkedHashMap<String,Integer>();
-	
-	Stack<Photo> photoHistory = new Stack<Photo>();
-		
-	int mode = 1;
-	
-	private int photoIdSelected;
-	
-	public void setImage() throws IOException{
-		
-	// Creation of the Date the User Inputs	
-	LocalDate sDate = dateInput.getValue();
-	
-	// Formation of apiKey for the date entered
-	String apiKey = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="+sDate+
-	"&api_key=Ri7l2K0C8IcYrKQRAtbYGekgYZekkJqS8G1ZMBkU";
-	
-	
-	//URL Connection
-	URL url = new URL(apiKey.toString());
-	
-	conn = (HttpURLConnection) url.openConnection();
-	
-	conn.setRequestMethod("GET");
-	
-	conn.connect();
-	
-	// Result returned from the API
-	StringBuffer apiResult = new StringBuffer();
-	
-	Scanner scan = new Scanner(url.openStream());
-	
-	while (scan.hasNext()) {
-		
-		apiResult.append(scan.nextLine());
-		
-	}
-	
-	scan.close();
-	
-	//JSON String from the Result Returned
-	String apiJSON = apiResult.toString();
+    /**
+     * photos objects stored as photo elements in an array list.
+     * Establish Connection to API.
+     * Store PhotoIDs as a linked Hashmap to recall photos from LinkedHashmap idToPhoto.
+     * PhotoHistory stored as stack to easily reverse order.
+     * int mode determines if viewing keyset in original order or reversed order. @historyButton()
+     * int photoIDSelected is key of photo selected from Photolist
+     */
 
-	JSONObject apiReturn = new JSONObject(apiJSON);
-	
-	JSONArray apiPhotos = apiReturn.getJSONArray("photos");
-	
-	photos.clear();
-	
-	// Setting the Photos in the Photo List from JSON for API
-	for (int i = 0;i<apiPhotos.length();i++) {
-		
-		JSONObject apiPhoto = (JSONObject)apiPhotos.get(i);
-		
-		Photo rPhoto = new Photo();
-		
-		rPhoto.setId(Integer.toString(apiPhoto.getInt("id")));
-		
-		rPhoto.setSol(Integer.toString(apiPhoto.getInt("sol")));
-		
-		JSONObject camera  = (JSONObject) apiPhoto.get("camera");
-		
-		rPhoto.setCameraId(Integer.toString(camera.getInt("id")));
-		
-		rPhoto.setRoverId(Integer.toString(camera.getInt("rover_id")));
-		
-		rPhoto.setCameraName(camera.getString("full_name"));
-		
-		rPhoto.setImgSrc(apiPhoto.getString("img_src"));
-		
-		rPhoto.setDate(apiPhoto.getString("earth_date"));
-		
-		JSONObject rover = (JSONObject) apiPhoto.get("rover");
-		
-		rPhoto.setRoverName(rover.getString("name"));
-		
-		rPhoto.setRoverStatus(rover.getString("status"));
-		
-		rPhoto.setLandD(rover.getString("landing_date"));
-		
-		rPhoto.setLaunchD(rover.getString("launch_date"));
-		
-		photos.add(rPhoto);		
-	}
-	
-	// Change our Data Structure
-	idToPhoto.clear();
-	
-	for (int i = 0;i<photos.size();i++) {
-		
-		idToPhoto.put(photos.get(i).getId(), i);
-		
-	}
-	
-	// Update the Image List
-	photoList.getItems().clear();
-	
-	photoList.getItems().addAll(idToPhoto.keySet());
-	
-	conn.disconnect();
-	
-	}
-	// Update the Image after selection
-	public void imageSelect() throws IOException {
-		
-		copyButton.setText("Copy");
-		
-		photoIdSelected = photoList.getSelectionModel().getSelectedIndex();
-		
-		if (photoIdSelected >=0) {
-		Image image = new Image(photos.get(photoIdSelected).getImgSrc());
-		
-		imageV.setImage(image);
-		
-		imageInfo.setText("Photo ID: "+photos.get(photoIdSelected).getId()+"\n"+
-		"Sol: "+photos.get(photoIdSelected).getSol()+"\n"
-		+"Camera ID: "+photos.get(photoIdSelected).getCameraId()+"\n"
-		+"Camera Name: "+photos.get(photoIdSelected).getCameraName()+"\n"
-		+"Rover ID: "+photos.get(photoIdSelected).getRoverId()+"\n"
-		+"Date: "+photos.get(photoIdSelected).getDate()+"\n"
-		+"Rover Name: "+photos.get(photoIdSelected).getRoverName()+"\n"
-		+"Rover Landing Date: "+photos.get(photoIdSelected).getLandD()+"\n"
-		+"Rover Launching Date: "+photos.get(photoIdSelected).getLaunchD()+"\n"
-		+"Rover Status: "+photos.get(photoIdSelected).getRoverStatus()+"\n"
-		);
-		
-		imgSrcText.setText(photos.get(photoIdSelected).getImgSrc());
-		
-		// If Mode is greater than 1 than we are in normal viewing mode so need to push to histroy
-		if(mode>0)
-		photoHistory.push(photos.get(photoIdSelected));
-		}
-	}
-	
-	public void downloadImage() throws IOException {
+    ArrayList<Photo>photos = new ArrayList<Photo>();
+
+    HttpURLConnection conn;
+
+    HashMap<String, Integer> idToPhoto = new LinkedHashMap<String,Integer>();
+
+    Stack<Photo> photoHistory = new Stack<Photo>();
+
+    int mode = 1;
+
+    private int photoIdSelected;
+
+    /**
+     * setImage() generates Photo Objects from the JSON Result that is returned from the API. The Photo generated is added to
+     *      the photos ArrayList. The PhotoIDs are then paired with the index of the photo object in photos arraylist.
+     *      JavaFX ListView @fx:id photoList then displays the photoIDs as a list. An HTTP Connection is established
+     *      to the generated URL. The URL generated contains the date specified from dateInput. String Buffer holds
+     *      the generated URL. Scanner is used to read JSON data from API URL. String JSON then converts JSON data
+     *      into a usable string. String is then parsed and builds photo objects. The information is then sorted
+     *      into instance variables of the specific photo objects. If the user wants to view a new date, the data
+     *      is then cleared. In the event a new list is generated, new IDs are assigned to new photos from the new
+     *      specified date.
+     */
+
+    public void setImage() throws IOException{
+
+        // Creation of the Date the User Inputs
+        LocalDate sDate = dateInput.getValue();
+
+        // Formation of apiKey for the date entered
+        String apiKey = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="+sDate+
+                "&api_key=Ri7l2K0C8IcYrKQRAtbYGekgYZekkJqS8G1ZMBkU";
+
+
+        //URL Connection
+        URL url = new URL(apiKey.toString());
+
+        conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("GET");
+
+        conn.connect();
+
+        // Result returned from the API
+        StringBuffer apiResult = new StringBuffer();
+
+        Scanner scan = new Scanner(url.openStream());
+
+        while (scan.hasNext()) {
+
+            apiResult.append(scan.nextLine());
+
+        }
+
+        scan.close();
+
+        //JSON String from the Result Returned
+        String apiJSON = apiResult.toString();
+
+        JSONObject apiReturn = new JSONObject(apiJSON);
+
+        JSONArray apiPhotos = apiReturn.getJSONArray("photos");
+
+        photos.clear();
+
+        // Setting the Photos in the Photo List from JSON for API
+        for (int i = 0;i<apiPhotos.length();i++) {
+
+            JSONObject apiPhoto = (JSONObject)apiPhotos.get(i);
+
+            Photo rPhoto = new Photo();
+
+            rPhoto.setId(Integer.toString(apiPhoto.getInt("id")));
+
+            rPhoto.setSol(Integer.toString(apiPhoto.getInt("sol")));
+
+            JSONObject camera  = (JSONObject) apiPhoto.get("camera");
+
+            rPhoto.setCameraId(Integer.toString(camera.getInt("id")));
+
+            rPhoto.setRoverId(Integer.toString(camera.getInt("rover_id")));
+
+            rPhoto.setCameraName(camera.getString("full_name"));
+
+            rPhoto.setImgSrc(apiPhoto.getString("img_src"));
+
+            rPhoto.setDate(apiPhoto.getString("earth_date"));
+
+            JSONObject rover = (JSONObject) apiPhoto.get("rover");
+
+            rPhoto.setRoverName(rover.getString("name"));
+
+            rPhoto.setRoverStatus(rover.getString("status"));
+
+            rPhoto.setLandD(rover.getString("landing_date"));
+
+            rPhoto.setLaunchD(rover.getString("launch_date"));
+
+            photos.add(rPhoto);
+        }
+
+        // Change Data Structure
+        idToPhoto.clear();
+
+        for (int i = 0;i<photos.size();i++) {
+
+            idToPhoto.put(photos.get(i).getId(), i);
+
+        }
+
+        // Update the Image List
+        photoList.getItems().clear();
+
+        photoList.getItems().addAll(idToPhoto.keySet());
+
+        conn.disconnect();
+
+    }
+    // Update the Image after selection
+
+    /**
+     * imageSelect() sets JavaFX imageView @fx:id imageV to the selected image. Selected image ID is pulled from
+     *      list generated by method setImage(). imageInfo pulls photo metadata using specified methods from
+     *      Photo.java Class. In the event mode is -1, the source for the methods changes to the stack.
+     */
+
+    public void imageSelect() throws IOException {
+
+        copyButton.setText("Copy");
+
+        photoIdSelected = photoList.getSelectionModel().getSelectedIndex();
+
+        if (photoIdSelected >=0) {
+            Image image = new Image(photos.get(photoIdSelected).getImgSrc());
+
+            imageV.setImage(image);
+
+            imageInfo.setText("Photo ID: "+photos.get(photoIdSelected).getId()+"\n"+
+                    "Sol: "+photos.get(photoIdSelected).getSol()+"\n"
+                    +"Camera ID: "+photos.get(photoIdSelected).getCameraId()+"\n"
+                    +"Camera Name: "+photos.get(photoIdSelected).getCameraName()+"\n"
+                    +"Rover ID: "+photos.get(photoIdSelected).getRoverId()+"\n"
+                    +"Date: "+photos.get(photoIdSelected).getDate()+"\n"
+                    +"Rover Name: "+photos.get(photoIdSelected).getRoverName()+"\n"
+                    +"Rover Landing Date: "+photos.get(photoIdSelected).getLandD()+"\n"
+                    +"Rover Launching Date: "+photos.get(photoIdSelected).getLaunchD()+"\n"
+                    +"Rover Status: "+photos.get(photoIdSelected).getRoverStatus()+"\n"
+            );
+
+            imgSrcText.setText(photos.get(photoIdSelected).getImgSrc());
+
+            // If Mode is greater than 1 than we are in normal viewing mode so need to push to histroy
+            if(mode>0)
+                photoHistory.push(photos.get(photoIdSelected));
+        }
+    }
+
+    /**
+     * downloadImage() sets a new URL as the URL from the selected image. It then opens the directory selector GUI.
+     * The selected directory (folder) is then saved as a file. An input stream is opened and recalls the image from
+     * the url specified. The output stream is then opened and the location is then fed in to the designated pathname
+     * provided by the directory selector gui. The output stream writer then writes the bytes array directly into
+     * the file. Both the input and output streams are then closed.
+     */
+
+    public void downloadImage() throws IOException {
         // Sets the URL
         URL url = new URL(photos.get(photoIdSelected).getImgSrc());
 
-        
         DirectoryChooser f = new DirectoryChooser();
-        
+
         Stage s = (Stage) anchorpane.getScene().getWindow();
-        
+
         File f1  = f.showDialog(s);
-        
+
         String filePath = f1.getAbsolutePath();
-        
-        
+
+
         // Open input stream to initiate transfer
         InputStream is = url.openStream();
 
@@ -224,104 +271,124 @@ public class Controller {
 
         // Close both streams.
         is.close();
-        os.close(); 
-        
-    }
-	// Copy to Clipboard Text
-	public void copyText() throws InterruptedException {
-		
-		Clipboard clip = Clipboard.getSystemClipboard();
-		
-		ClipboardContent cont = new ClipboardContent();
-		
-		cont.putString(photos.get(photoIdSelected).getImgSrc()); //Change ImgSrc to the proper variable or method to get the link
-		
-		clip.setContent(cont);
-		
-		copyButton.setText("Copied!");
-		
-	}
+        os.close();
 
-	public void historyButtonM() throws IOException {
-		
-		mode*=-1;
-		
-		historyButton.setVisited(false);
-		
-		if (mode>0) {
-			
-			photoList.getItems().clear();
-			
-			idToPhoto.clear();
-			
-			historyButton.setText("View History");
-			
-			setImage();
-			
-		}
-		else {
-			
-		getHistory();
-			
-		}
-		
-	}
-	
-	public void getHistory() throws IOException {
-		
-		photos.clear();
-		
-		photoList.getItems().clear();
-		
-		idToPhoto.clear();
-		
-		Queue<Photo>auxS = new LinkedList<Photo>();
-		
-		int stackS = photoHistory.size();
-		
-		// Stack to Queue Twice to keep the same order of photoHistory (Stack)
-		
-		for (int i = 0;i<stackS;i++) {
-			
-			photos.add(photoHistory.peek());
-			
-			idToPhoto.put(photoHistory.peek().getId(), i);
-			
-			auxS.add(photoHistory.pop());
-			
-		}
-		
-		for (int i = 0;i<stackS;i++) {
-			
-			photoHistory.push(auxS.remove());
-			
-		}
-		
-		for (int i = 0;i<stackS;i++) {
-			
-			auxS.add(photoHistory.pop());;
-			
-		}
-		
-		for (int i = 0;i<stackS;i++) {
-			
-			photoHistory.push(auxS.remove());
-			
-		}
-		
-		
-		for (int i = 0;i<photoHistory.size();i++) {
-			
-			idToPhoto.put(photoHistory.get(i).getId(), i);
-			
-		}
-		
-		photoList.getItems().addAll(idToPhoto.keySet());
-		
-		historyButton.setText("Photos");
-			
-		
-	}
-	
-	
+    }
+
+    /**
+     * copyText() pulls the String from JavaFX TextField @fx:id imgSrcText and puts that string into the clipboard.
+     */
+    // Copy to Clipboard Text
+    public void copyText() throws InterruptedException {
+
+        Clipboard clip = Clipboard.getSystemClipboard();
+
+        ClipboardContent cont = new ClipboardContent();
+
+        cont.putString(photos.get(photoIdSelected).getImgSrc()); //Change ImgSrc to the proper variable or method to get the link
+
+        clip.setContent(cont);
+
+        copyButton.setText("Copied!");
+
+    }
+
+    /**
+     * historyButtonM() is a method that gets called if the hyperlink is clicked. It changes JavaFX ListView @fx:id
+     * photoList to the stack. Allowing the user to switch between photos and their previously viewed photos.
+     */
+    public void historyButtonM() throws IOException {
+
+        mode*=-1;
+
+        historyButton.setVisited(false);
+
+        if (mode>0) {
+
+            photoList.getItems().clear();
+
+            idToPhoto.clear();
+
+            historyButton.setText("View History");
+
+            setImage();
+
+        }
+        else {
+
+            getHistory();
+
+        }
+
+    }
+
+    /**
+     * getHistory() Records the PhotoIds into a stack of previously selected images.
+     */
+
+    public void getHistory() throws IOException {
+
+        photos.clear();
+
+        photoList.getItems().clear();
+
+        idToPhoto.clear();
+
+        Queue<Photo>auxS = new LinkedList<Photo>();
+
+        int stackS = photoHistory.size();
+
+        // Stack to Queue Twice to keep the same order of photoHistory (Stack)
+
+        for (int i = 0;i<stackS;i++) {
+
+            photos.add(photoHistory.peek());
+
+            idToPhoto.put(photoHistory.peek().getId(), i);
+
+            auxS.add(photoHistory.pop());
+
+        }
+
+        for (int i = 0;i<stackS;i++) {
+
+            photoHistory.push(auxS.remove());
+
+        }
+
+        for (int i = 0;i<stackS;i++) {
+
+            auxS.add(photoHistory.pop());;
+
+        }
+
+        for (int i = 0;i<stackS;i++) {
+
+            photoHistory.push(auxS.remove());
+
+        }
+
+
+        for (int i = 0;i<photoHistory.size();i++) {
+
+            idToPhoto.put(photoHistory.get(i).getId(), i);
+
+        }
+
+        photoList.getItems().addAll(idToPhoto.keySet());
+
+        historyButton.setText("Photos");
+
+
+    }
+
+
 }
+// Citations
+// http://tutorials.jenkov.com/java-io/inputstream.html
+// https://www.programiz.com/java-programming/outputstream
+// https://docs.oracle.com/javase/7/docs/api/java/nio/file/Files.html#copy(java.nio.file.Path,%20java.io.OutputStream)
+
+// https://stackoverflow.com/questions/47654918/javafx-directory-chooser-how-to-get-the-path-from-directory
+// http://tutorials.jenkov.com/javafx/directorychooser.html
+// https://www.javatpoint.com/how-to-create-a-new-folder-in-java
--- src/application/Photo.java
+++ src/application/Photo.java
@@ -1,105 +1,173 @@
 package application;
 
+/**
+ * Collection of object-oriented aspects of java
+ */
 public class Photo {
 
-	private String id;
-	
-	private String sol;
-	
-	private String cameraId;
-	
-	private String cameraName;
-	
-	private String roverId;
-	
-	private String imgSrc;
-	
-	private String date;
-	
-	private String roverName;
-	
-	private String roverLandingD;
-	
-	private String roverLaunchD;
-	
-	private String roverStatus;
-	
-	
-	public Photo(){
-		
-	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
-	public void setSol(String sol) {
-		this.sol = sol;
-	}
-	public void setCameraId(String cameraId) {
-		this.cameraId = cameraId;
-	}
-	public void setCameraName(String cameraName) {
-		this.cameraName = cameraName;
-	}
-	public void setRoverId(String roverId) {
-		this.roverId = roverId;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	public void setLaunchD(String lD) {
-		this.roverLaunchD = lD;
-	}
-	public void setLandD(String ld) {
-		this.roverLandingD = ld;
-	}
-	public void setRoverStatus(String roverS) {
-		this.roverStatus = roverS;
-	}
-	public void setImgSrc(String imgSrc) {
-		this.imgSrc = imgSrc; 
-	}
-	public void setRoverName(String roverName) {
-		this.roverName = roverName;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	public String getSol() {
-		return sol;
-	}
-	public String getCameraId() {
-		return cameraId;
-	}
-	public String getCameraName() {
-		return cameraName;
-	}
-	public String getRoverId() {
-		return roverId;
-	}
-	public String getDate() {
-		return date;
-	}
-	public String getLaunchD() {
-		return roverLaunchD;
-	}
-	public String getLandD() {
-		return roverLandingD;
-	}
-	public String getRoverStatus() {
-		return roverStatus;
-	}
-	public String getImgSrc() {
-		return imgSrc;
-	}
-	public String getRoverName() {
-		return roverName;
-	}
-	
-	public String toString() {
-		
-		return "ID: "+getId()+" Src: "+getImgSrc()+"\n";
-		
-	}
+    private String id;
+
+    private String sol;
+
+    private String cameraId;
+
+    private String cameraName;
+
+    private String roverId;
+
+    private String imgSrc;
+
+    private String date;
+
+    private String roverName;
+
+    private String roverLandingD;
+
+    private String roverLaunchD;
+
+    private String roverStatus;
+
+
+    public Photo(){
+
+    }
+
+    /**
+     * Sets instance variable "id" to @param id.
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+    /**
+     * Sets instance variable "sol" to @param sol.
+     */
+    public void setSol(String sol) {
+        this.sol = sol;
+    }
+    /**
+     * Sets instance variable "cameraID" to @param cameraID.
+     */
+    public void setCameraId(String cameraId) {
+        this.cameraId = cameraId;
+    }
+    /**
+     * Sets instance variable "cameraName" to @param cameraName.
+     */
+    public void setCameraName(String cameraName) {
+        this.cameraName = cameraName;
+    }
+    /**
+     * Sets instance variable "roverID" to @param roverID.
+     */
+    public void setRoverId(String roverId) {
+        this.roverId = roverId;
+    }
+    /**
+     * Sets instance variable "setDate" to @param setDate.
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+    /**
+     * Sets instance variable "setLaunchD" to @param setLaunchD.
+     */
+    public void setLaunchD(String lD) {
+        this.roverLaunchD = lD;
+    }
+    /**
+     * Sets instance variable "setLandD" to @param setLandD.
+     */
+    public void setLandD(String ld) {
+        this.roverLandingD = ld;
+    }
+    /**
+     * Sets instance variable "setRoverStatus" to @param setRoverStatus.
+     */
+    public void setRoverStatus(String roverS) {
+        this.roverStatus = roverS;
+    }
+    /**
+     * Sets instance variable "setImgSrc" to @param setImgSrc.
+     */
+    public void setImgSrc(String imgSrc) {
+        this.imgSrc = imgSrc;
+    }
+    /**
+     * Sets instance variable "setRoverName" to @param setRoverName.
+     */
+    public void setRoverName(String roverName) {
+        this.roverName = roverName;
+    }
+    /**
+     * Returns instance variable "getId" to @return getId.
+     */
+    public String getId() {
+        return id;
+    }
+    /**
+     * Returns instance variable "getSol" to @return getSol.
+     */
+    public String getSol() {
+        return sol;
+    }
+    /**
+     * Returns instance variable "getCameraID" to @return getCameraID.
+     */
+    public String getCameraId() {
+        return cameraId;
+    }
+    /**
+     * Returns instance variable "getCameraName" to @return getCameraName.
+     */
+    public String getCameraName() {
+        return cameraName;
+    }
+    /**
+     * Returns instance variable "getRoverId" to @return getRoverId.
+     */
+    public String getRoverId() {
+        return roverId;
+    }
+    /**
+     * Returns instance variable "getDate" to @return getDate.
+     */
+    public String getDate() {
+        return date;
+    }
+    /**
+     * Returns instance variable "getLaunchD" to @return getLaunchD.
+     */
+    public String getLaunchD() {
+        return roverLaunchD;
+    }
+    /**
+     * Returns instance variable "getLandD" to @return getLandD.
+     */
+    public String getLandD() {
+        return roverLandingD;
+    }
+    /**
+     * Returns instance variable "getRoverStatus" to @return getRoverStatus.
+     */
+    public String getRoverStatus() {
+        return roverStatus;
+    }
+    /**
+     * Returns instance variable "getImgSrc" to @return getImgSrc.
+     */
+    public String getImgSrc() {
+        return imgSrc;
+    }
+    /**
+     * Returns instance variable "getRoverName" to @return getRoverName.
+     */
+    public String getRoverName() {
+        return roverName;
+    }
+
+    public String toString() {
+
+        return "ID: "+getId()+" Src: "+getImgSrc()+"\n";
+
+    }
 }
